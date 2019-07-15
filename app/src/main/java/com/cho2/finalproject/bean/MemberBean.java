f75052a4bb3952763e8bc5cd76ce8198ab59dd6d
KimSuHyeon <sh975@naver.com>
2019-07-15 16:49:34 +0900

    Bean 만듬

---
+++ app/src/main/java/com/cho2/finalproject/bean/MemberBean.java
@@ -0,0 +1,12 @@
+package com.cho2.finalproject.bean;
+
+import java.util.List;
+
+public class MemberBean {
+    public String userEmail; //이메일
+    public String ImgIDuri; //사진 주소
+    public String Phonenum;//핸드폰 번호
+    public String name;//학생 이름
+
+
+}
---
+++ app/src/main/java/com/cho2/finalproject/bean/ReservationBean.java
@@ -0,0 +1,8 @@
+package com.cho2.finalproject.bean;
+
+public class ReservationBean {
+    public String date; //일자
+    public String building; //건물
+    public String room;     //호실
+    public String time ;    //시간
+}
