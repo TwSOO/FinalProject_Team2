887fdf889929e5303c126911cfc3199f8935c340
KimSuHyeon <sh975@naver.com>
2019-07-18 14:02:55 +0900

    예약내역 DB에 업로드 구현 완료

--- app/src/main/java/com/cho2/finalproject/bean/ReservationBean.java
+++ app/src/main/java/com/cho2/finalproject/bean/ReservationBean.java
@@ -3,15 +3,15 @@ package com.cho2.finalproject.bean;
 import java.io.Serializable;
 
 public class ReservationBean implements Serializable {
-    public static final int FIFTH = 1;
-    public static final int HUMAN = 2;
-    public static final int SCIENCE1 = 3;
-    public static final int SCIENCE2= 4;
+    public static final String FIFTH = "50주년기념관";
+    public static final String HUMAN = "인문사회관";
+    public static final String SCIENCE1 = "제1과학관";
+    public static final String SCIENCE2= "제2과학관";
 
     public String mReserveMonth; // 예약 월
     public String mReserveDay; // 예약 일
     public String mReserveTime; // 예약 시간
-    public int mReserveBuilding; //건물
+    public String mReserveBuilding; //건물
     public String mReserveRoom;     //호실
     public String mEmail;//예약한 사람 이메일
 
