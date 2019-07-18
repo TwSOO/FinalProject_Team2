83a0bbe50eb930418f05047b92b73fde3b1e3042
KimSuHyeon <sh975@naver.com>
2019-07-18 14:44:14 +0900

    firebase 폴더 아래의 InsertFirebase 클래스 안에 예약취소함수cancelReservation() 만듬

--- app/src/main/java/com/cho2/finalproject/MyPageActivity.java
+++ app/src/main/java/com/cho2/finalproject/MyPageActivity.java
@@ -9,6 +9,10 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.cho2.finalproject.bean.ReservationBean;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MyPageActivity extends AppCompatActivity {
 
     @Override
@@ -51,4 +55,5 @@ public class MyPageActivity extends AppCompatActivity {
     }
 
 
-}
+
+} // end class
--- app/src/main/java/com/cho2/finalproject/firebase/InsertFirebase.java
+++ app/src/main/java/com/cho2/finalproject/firebase/InsertFirebase.java
@@ -7,7 +7,9 @@ import androidx.annotation.Nullable;
 
 import com.cho2.finalproject.JoinAcitivity;
 import com.cho2.finalproject.bean.MemberBean;
+import com.cho2.finalproject.bean.ReservationBean;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 
@@ -24,5 +26,11 @@ public class InsertFirebase {
     public static String getUserIdFromUUID(String userEmail){
         long val = UUID.nameUUIDFromBytes(userEmail.getBytes()).getMostSignificantBits(); // 사용자 이메일을 고유 숫자값으로 바꿈
         return String.valueOf(val);
+    }
+
+    // 예약 취소 함수
+    public void cancelReservation(ReservationBean reservationBean){
+        DatabaseReference dbRef=mFirebaseDatabase.getReference();
+        dbRef.child("reservations").child(reservationBean.mReserveBuilding).child(reservationBean.mReserveRoom+"호").child(reservationBean.makeDate()).child(reservationBean.mReserveTime).removeValue();
     }
 } // end InserFirebase
