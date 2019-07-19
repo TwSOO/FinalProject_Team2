36c36dadd8cd12af7492e6f2dfe7ddad09dabac6
KimSuHyeon <sh975@naver.com>
2019-07-19 15:02:53 +0900

    로그인한 멤버 받아와야함

--- app/src/main/java/com/cho2/finalproject/AdminActivity.java
+++ app/src/main/java/com/cho2/finalproject/AdminActivity.java
@@ -90,7 +90,7 @@ public class AdminActivity extends AppCompatActivity {
             // 리스트뷰에 어댑터 설정
             adminListView.setAdapter(myPageAdapter);
         }
-
+        Log.e("AdminActitivity", mReservationCompleatBeanList.toString());
 
         arrayList = new ArrayList<>();
         arrayList.add("7월");
@@ -120,29 +120,5 @@ public class AdminActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-       /* findViewById(R.id.btnDetail).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder alertDialog = new AlertDialog.Builder(AdminActivity.this);
-                alertDialog.setTitle("예약정보"); //이것도 가운데로 정렬하고 싶음.
-                alertDialog.setMessage("시간:10:00~11:00\n예약을 취소하시겠습니까?"); //가운데 정렬로 하고싶음.
-                alertDialog.setPositiveButton("예약취소", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        // 예약취소하기(실제로 취소되도록 연결)
-                    }
-                });
-                alertDialog.setNeutralButton("아니오", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        // 아무일 안일어남.
-                    }
-                });
-                alertDialog.show();
-            }
-        });*/
     }
 }
