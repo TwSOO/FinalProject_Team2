ef9b934375759c42a02afd71b33a131333526ec5
uzen.tim <itbong@amorepacific.com>
2019-07-19 13:01:50 +0900

    예약하기 추가.

--- app/src/main/java/com/cho2/finalproject/MyPageActivity.java
+++ app/src/main/java/com/cho2/finalproject/MyPageActivity.java
@@ -1,49 +1,48 @@
 package com.cho2.finalproject;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.cho2.finalproject.adapter.TImeAdapter;
-import com.cho2.finalproject.bean.ReservationBean;
+import com.cho2.finalproject.adapter.MyPageAdapter;
+import com.cho2.finalproject.bean.MemberBean;
+import com.cho2.finalproject.firebase.InsertFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MyPageActivity extends AppCompatActivity {
 
     private FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
+    private ListView mListMyPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
 
-        ListView listMyPage = findViewById(R.id.listMyPage);
+        mListMyPage = findViewById(R.id.listMyPage);
 
+        String uuid = InsertFirebase.getUserIdFromUUID( FirebaseAuth.getInstance().getCurrentUser().getEmail() );
 
-//        mFirebaseDatabase.getReference().child("reservations").child(mReservationBean.step1BuildName)
-//                .child(mReservationBean.step2Day).child(mReservationBean.step3RoomName).addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                ReservationBean reservationBean = dataSnapshot.getValue(ReservationBean.class);
-//
-//                mTimeAdapter = new TImeAdapter(ReservationActivity.this, reservationBean);
-//                mListView.setAdapter(mTimeAdapter);
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {}
-//        });
+        mFirebaseDatabase.getReference().child("members").child(uuid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                MemberBean memberBean = dataSnapshot.getValue(MemberBean.class);
+
+                if(memberBean.reservationList != null) {
+                    MyPageAdapter myPageAdapter = new MyPageAdapter(MyPageActivity.this, memberBean.reservationList);
+                    mListMyPage.setAdapter(myPageAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {}
+        });
     }
 
 
--- app/src/main/java/com/cho2/finalproject/adapter/MyPageAdapter.java
+++ app/src/main/java/com/cho2/finalproject/adapter/MyPageAdapter.java
@@ -26,27 +26,26 @@ import java.util.List;
 public class MyPageAdapter extends BaseAdapter {
 
     private Context mContext;
-    private ReservationBean mReservationBean;
-    private List<TimeBean> mTimeList;
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference dbRef;
+    List<ReservationBean> mReservationList;
 
-    public MyPageAdapter(Context context, ReservationBean reservationBean) {
+
+    public MyPageAdapter(Context context, List<ReservationBean> reservationList) {
         mContext = context;
-        mReservationBean = reservationBean;
-        mTimeList = mReservationBean.getTimeList();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         dbRef = mFirebaseDatabase.getReference();
+        mReservationList = reservationList;
     }
 
     @Override
     public int getCount() {
-        return mTimeList.size();
+        return mReservationList.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return mTimeList.get(i);
+        return mReservationList.get(i);
     }
 
     @Override
@@ -57,72 +56,18 @@ public class MyPageAdapter extends BaseAdapter {
     @Override
     public View getView(final int position, View view, ViewGroup viewGroup) {
         LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        view = inflater.inflate(R.layout.view_resv, null);
+        view = inflater.inflate(R.layout.view_resv_item, null);
 
-        final TimeBean timeBean = mTimeList.get(position);
+        final ReservationBean reservationBean = mReservationList.get(position);
 
         // xml파일을 맵핑
-        TextView txtTime = view.findViewById(R.id.txtTime);
-        TextView txtReservation = view.findViewById(R.id.txtResvState);
-        Button btnReservation = view.findViewById(R.id.btnReserve);
+//        TextView txtTime = view.findViewById(R.id.txtTime);
+//        TextView txtReservation = view.findViewById(R.id.txtResvState);
+//        Button btnReservation = view.findViewById(R.id.btnReserve);
 
-        // 아이템 정보 뷰에 담기 + 버튼 리스너 설정
-        txtTime.setText(timeBean.timeTitle);
-        if(timeBean.isReservation){
-            txtReservation.setText("예약완료");
-            btnReservation.setVisibility(View.INVISIBLE);
-        }
-        else{
-            txtReservation.setText("예약가능");
-            btnReservation.setVisibility(View.VISIBLE);
-        }
-        //예약하기 버튼 클릭
-        btnReservation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                reservation(position, timeBean);
-
-            }
-        });
 
         return view;
-    }
-
-
-    private void reservation(final int position, final TimeBean timeBean){
-
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
-        alertDialog.setTitle("예약확인"); //이것도 가운데로 정렬하고 싶음.
-        alertDialog.setMessage("시간: " + timeBean.timeTitle + "\n예약하시겠습니까?"); //가운데 정렬로 하고싶음.
-        alertDialog.setPositiveButton("예", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-                timeBean.isReservation = true;
-                timeBean.userId = FirebaseAuth.getInstance().getCurrentUser().getEmail();
-
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd HH:mm");
-                try {
-                    //시작시간
-                    timeBean.startMilliTime = sdf.parse(mReservationBean.step2Day + " " + timeBean.timeTitle).getTime();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                //기존 item 교체
-                mReservationBean.getTimeList().set(position, timeBean);
-
-                dbRef.child("reservations").child(mReservationBean.step1BuildName).child(mReservationBean.step2Day).child(mReservationBean.step3RoomName).setValue(mReservationBean);
-                Toast.makeText(mContext,"예약 완료", Toast.LENGTH_LONG).show();
-
-                Intent intent = new Intent(mContext, MyPageActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                mContext.startActivity(intent);
-            }
-        });
-        alertDialog.setNegativeButton("아니오", null);
-        alertDialog.create().show();
-
     }
 
 }
